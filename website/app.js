b1b3094cd71bbb6aab4e666db383bfe18b01ef4a
AhmedIbrahiiiim <a7medmo7amed.288@gmail.com>
2021-11-18 09:49:24 +0200

    Unit Choosing Are Added

--- website/app.js
+++ website/app.js
@@ -1,11 +1,13 @@
 /* Global Variables */
 const apiUrl = "http://localhost:4500/";
-const apiKey = "&appid=c3a1e8ac9f7c554e83f622b9bcf966fe&units=metric";
+const apiKeyMetric = "&appid=c3a1e8ac9f7c554e83f622b9bcf966fe&units=metric";
+const apiKeyImperial = "&appid=c3a1e8ac9f7c554e83f622b9bcf966fe&units=imperial";
 let zip = document.getElementById("zip");
 let feeling = document.getElementById("feeling");
 let date = document.getElementById("date");
 let temp = document.getElementById("temp");
 let content = document.getElementById("content");
+let unit = document.getElementById("unit");
 let btn = document.getElementById("generate");
 let errorFound = function (error) {
   console.log(`Some Error Occurred`, error);
@@ -35,7 +37,7 @@ function gen() {
       if (zipInfos.cod != 200) {
         return alert(zipInfos.message);
       } else {
-        // getting the Temperature & post data to the server for saving it then display it for the user
+        // getting the Temperature & post data to the server for saving it the display it for the user
         data.temp = zipInfos.list[0].main.temp;
         postDataToServer(data);
       }
@@ -44,9 +46,20 @@ function gen() {
 }
 // Get zip Code Information Function
 async function getZipCodeInformation(zipCode) {
-  let apiLink = await fetch(
-    `http://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}${apiKey}`
-  );
+  let apiLink;
+  if (unit.value == "celsius") {
+    // To Get The Weather By Celsius Degrees
+    apiLink = await fetch(
+      `http://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}${apiKeyMetric}`
+    );
+  } else if (unit.value == "fahrenheit") {
+    // To Get The Weather By Fahrenheit Degrees
+    apiLink = await fetch(
+      `http://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}${apiKeyImperial}`
+    );
+  } else {
+    window.alert(`Please Select a Unit`);
+  }
   return apiLink.json();
 }
 // Post Data Function
@@ -79,8 +92,22 @@ async function updatePage() {
   try {
     res.json().then(function (data) {
       date.innerHTML = `Date is: <span style="font-weight: 700;">${data.date}</span>`;
-      temp.innerHTML = `Temperature is: <span style="font-weight: 700;">${data.temp}</span> <span style="font-weight: bold;">&deg;</span><span style="font-weight: 400">C</span>`;
-      content.innerHTML = `My feeling is: <span style="font-weight: 700;">${data.content}</span>`;
+      // To See The Unit That The User Chose To Know What Unit To Display
+      if (unit.value == "celsius") {
+        // To Show The Weather By Celsius Degrees (C)
+        temp.innerHTML = `Temperature is: <span style="font-weight: 700;">${data.temp}</span> <span style="font-weight: bold;">&deg;</span><span style="font-weight: 400">C</span>`;
+      } else if (unit.value == "fahrenheit") {
+        // To Show The Weather By Fahrenheit Degrees (F)
+        temp.innerHTML = `Temperature is: <span style="font-weight: 700;">${data.temp}</span> <span style="font-weight: bold;">&deg;</span><span style="font-weight: 400">F</span>`;
+      } else {
+        window.alert(`Please Select a Unit`);
+      }
+      // To See If The TextArea Has Text Or Not
+      if (feeling.value === "") {
+        content.innerHTML = `My feeling is: <span style="font-weight: 700;">N/A</span>`;
+      } else {
+        content.innerHTML = `My feeling is: <span style="font-weight: 700;">${data.content}</span>`;
+      }
     });
   } catch (error) {
     errorFound(error);
